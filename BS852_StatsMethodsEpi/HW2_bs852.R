d9ad5b0d89655a238d583041c4a9b56a07da3f45
Mitchell Henschel <me@mitchellhenschel.com>
2022-09-22 19:12:44 -0400

    Progress 852 HW2

--- BS852_StatsMethodsEpi/HW2_bs852.R
+++ BS852_StatsMethodsEpi/HW2_bs852.R
@@ -18,3 +18,21 @@ mantelhaen.test(smkrs)$estimate
 
 # 2
 heart <- read.csv("/home/elkip/Datasets/framdat2.txt", sep = "", na.strings = ".")
+heart2 <- heart[ which(heart$SEX == 2 & (heart$CHD == 0 | heart$CHD > 4)),]
+attach(heart2)
+age <- AGE
+age[ which( AGE >= 35 & AGE < 45)] <- 1
+age[ which( AGE >= 45 & AGE < 55)] <- 2
+age[ which( AGE >= 55 & AGE < 65)] <- 3
+age[ which( AGE >= 65 & AGE < 75)] <- 4
+
+chd_sw = CHD < 4
+glucose = GLI == 0
+fram_data <- table(glucose, chd_sw, age)
+res <- epi.2by2(dat=fram_data, method = "cohort.count", conf.level = .95)
+
+names(summary(res)$massoc.detail)
+summary(res)$massoc.detail$OR.crude.wal$est
+summary(res)$massoc.detail$OR.strata.wald$est
+summary(res)$massoc.detail$OR.mh.wald$est
+summary(res)$massoc.detail$chi2.mh
