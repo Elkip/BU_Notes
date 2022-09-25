cb8c29d1ed3eed39585be72c0c98f500b5434907
Mitch <me@mitchellhenschel.com>
2022-09-25 14:29:42 -0400

    Finished BS852 HW2

--- BS852_StatsMethodsEpi/HW2_bs852.R
+++ BS852_StatsMethodsEpi/HW2_bs852.R
@@ -1,6 +1,8 @@
 library(epiR)
+library(epitools)
 
 # 1
+
 smkrs <- c(41, 39, 192, 199, 145, 36, 205, 102)
 dim(smkrs) <- c(2,2,2)
 
@@ -15,24 +17,93 @@ summary(mi.BD)$massoc.detail$OR.homog.brday
 
 mantelhaen.test(smkrs)
 mantelhaen.test(smkrs)$estimate
+oddsratio(smkrs[[1]])
 
 # 2
 heart <- read.csv("/home/elkip/Datasets/framdat2.txt", sep = "", na.strings = ".")
-heart2 <- heart[ which(heart$SEX == 2 & (heart$CHD == 0 | heart$CHD > 4)),]
+# Keep observations for women (SEX ==2), after exam 4 or no event (CHD = 0 or CHD > 4)
+# And only existing data for GLI
+heart2 <- heart[ which(!is.na(heart$GLI) & heart$SEX == 2 & (heart$CHD == 0 | heart$CHD > 4)),]
 attach(heart2)
-age <- AGE
-age[ which( AGE >= 35 & AGE < 45)] <- 1
-age[ which( AGE >= 45 & AGE < 55)] <- 2
-age[ which( AGE >= 55 & AGE < 65)] <- 3
-age[ which( AGE >= 65 & AGE < 75)] <- 4
 
-chd_sw = CHD < 4
-glucose = GLI == 0
-fram_data <- table(glucose, chd_sw, age)
-res <- epi.2by2(dat=fram_data, method = "cohort.count", conf.level = .95)
+#heart2 <- heart[ which(heart$SEX == 2 & (heart$CHD == 0 | heart$CHD > 4)),]
+#attach(heart2)
 
+## Create Bins for Age
+agegrp <- floor((AGE - 35)/10 + 1)
+
+# Indicator Variable
+chd_sw = CHD > 4
+# Create labels
+chd_sw <- factor(chd_sw, levels = c(TRUE, FALSE), labels = c("Event", "No event"))
+glci <- factor(GLI, levels = c(1,0), labels = c("GI", "Not GI"))
+fram.data <- table(glci, chd_sw, agegrp)
+res <- epi.2by2(dat=fram.data, method = "cohort.count", conf.level = .95)
+
+res
 names(summary(res)$massoc.detail)
 summary(res)$massoc.detail$OR.crude.wal$est
 summary(res)$massoc.detail$OR.strata.wald$est
 summary(res)$massoc.detail$OR.mh.wald$est
 summary(res)$massoc.detail$chi2.mh
+
+fram.data.ajd <- fram.data + .5
+mi.BD.adj <- epi.2by2(dat = fram.data.ajd, method = "case.control", conf.level = .95)
+
+summary(mi.BD.adj)$massoc.detail$OR.homog.woolf
+summary(mi.BD.adj)$massoc.detail$OR.homog.brday
+
+get.odds <- function(mtrxList) {
+  t_size = length(mtrxList)
+  a <- vector(length = t_size)
+  b <- vector(length = t_size)
+  c <- vector(length = t_size)
+  d <- vector(length = t_size)
+  n_total <- vector(length = t_size)
+  n0 <-  vector(length = t_size)
+  n1 <- vector(length = t_size)
+  m0 <- vector(length = t_size)
+  m1 <- vector(length = t_size)
+  mor_num = 0
+  mor_den = 0
+  mh_num = 0
+  mh_den = 0
+  
+  for(i in 1:t_size) {
+    mat = mtrxList[[i]]
+    # print(mat)
+    a[i] = mat[1,1]
+    b[i] = mat[1,2]
+    c[i] = mat[2,1]
+    d[i] = mat[2,2]
+    
+    n_total[i] <- a[i] + b[i] + c[i] + d[i]
+    n1[i] <- a[i] + c[i]
+    n0[i] <- b[i] + d[i]
+    m1[i] = a[i] + b[i]
+    m0[i] = c[i] + d[i]
+    
+    mor_num <- mor_num + (a[i]*d[i]/n_total[i])
+    mor_den <- mor_den + (b[i]*c[i]/n_total[i])
+    mh_num <- mh_num + ((a[i]*d[i] - b[i]*c[i]) / n_total[i])
+    mh_den <- mh_den + ((n0[i]*n1[i]*m0[i]*m1[i])/(n_total[i]^2*(n_total[i] - 1)))
+  
+    print(paste("Odds Ratio:", (((a[i]/c[i])/(b[i]/d[i])))))
+    } 
+  
+  mh <- mh_num^2 / mh_den 
+  print(mh)
+  print(pchisq(mh,t_size-1,lower.tail = F))
+  
+  mor <- mor_num / mor_den
+  print(paste("mOR", mor))
+  mor_ci_top <- mor^(1+1.96/sqrt(mh))
+  mor_ci_btm <- mor*(1-1.96/sqrt(mh))
+  print("Confidence interval:")
+  print(paste(mor_ci_btm, mor_ci_top))
+  
+}
+
+
+get.odds(list(smkrs[,,1], smkrs[,,2]))
+get.odds(list(fram.data[,,1], fram.data[,,2], fram.data[,,3], fram.data[,,4]))
