8bfc5a792d4acefd8f9cad6e66e15b0a0255959d
Mitch <me@mitchellhenschel.com>
2022-10-15 12:23:48 -0400

    852 HW5

--- BS852_StatsMethodsEpi/HW5_852_EpiStats.R
+++ BS852_StatsMethodsEpi/HW5_852_EpiStats.R
@@ -1,3 +1,5 @@
+library(aod) # inlcudes function wald.test()
+library(lmtest)
 lung <- read.table("/home/elkip/Datasets/Lungca.txt",header=T, na.strings=c("."))
 
 # 1
@@ -14,7 +16,7 @@ exp(.6875 - 1.96 * .3486)
 
 # 2 
 # a. Logistic Regression with columns 1:7
-log.adjusted_full <- glm(Y ~ X1 + X2 + X3 + X4 + X5 + X6 + X7, family=binomial, data = lung)
+log.full <- glm(Y ~ X1 + X2 + X3 + X4 + X5 + X6 + X7, family=binomial, data = lung)
 log.adjusted <- glm(Y ~ X2 + X3 + X4 + X5 + X6 + X7, family=binomial, data = lung)
 summary(log.adjusted_full)
 anova(log.adjusted, log.adjusted_full)
@@ -24,3 +26,30 @@ pchisq(5.281,1,lower.tail = F)
 exp(1.068)
 exp(1.068 - 1.96 * .47896)
 exp(1.068 + 1.96 * .47896)
+
+# 3
+# Crude analysis
+wald.test(b = coef(log.crude), Sigma = vcov(log.crude), Terms = 2)
+# Adjusted analysis
+confint.default(log.adjusted_full)
+exp(cbind(OR = coef(log.adjusted_full), confint.default(log.adjusted_full)))
+wald.test(b = coef(log.adjusted_full), Sigma = vcov(log.adjusted_full), Terms = 2)
+# Confounding analysis
+exp(coef(log.crude)["X1"])/exp(coef(log.adjusted_full)["X1"])
+
+# 4
+# Association between cell type and odds of death
+log.full_no_celltype <- glm(Y ~ X1 + X5 + X6 + X7, family=binomial, data = lung)
+log.adjusted_no_celltype <-  glm(Y ~ X5 + X6 + X7, family=binomial, data = lung)
+lrtest(log.adjusted_no_celltype, log.adjusted)
+anova(log.adjusted_no_celltype, log.adjusted)
+lrtest(log.full_no_celltype, log.full)
+anova(log.full_no_celltype, log.full)
+
+# 5 
+# Logistic Model X1-X8
+log.full_x8 <- glm(Y ~ X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8, family=binomial, data = lung)
+
+exp(-0.079387*(60-45))
+exp(-0.079387-1.96 * 0.015648 *(60-45))
+exp(-0.079387+1.96 * 0.015648 *(60-45))
