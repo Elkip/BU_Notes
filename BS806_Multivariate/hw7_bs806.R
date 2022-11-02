9fabe8eb1b73e647f82f452e5e40bf0a6e42dd6a
Mitch <me@mitchellhenschel.com>
2022-11-01 20:21:21 -0400

    Stuff

--- BS806_Multivariate/hw7_bs806.R
+++ BS806_Multivariate/hw7_bs806.R
@@ -3,8 +3,10 @@ library(tree)
 fhs <- read.csv("/home/elkip/Datasets/FHS_data.csv")
 fhs <- na.omit(fhs)
 
-set.seed(1)
-sd.est = sqrt(6194)
+# 1
+# 1.2 Var = Residual mean deviance
+var = 15690000 / 2533
+sd.est = sqrt(var)
 mean.node1 = 574.8
 mean.node2 = 512.3
 mean.node3 = 444.9
@@ -15,6 +17,7 @@ n.node2 = fhs[fhs$SEX == 1 & fhs$AGE >= 48.5,]
 n.node3 = fhs[fhs$SEX == 2 & fhs$AGE < 51.5,]
 n.node4 = fhs[fhs$SEX == 2 & fhs$AGE >= 51.5,]
 
+set.seed(1)
 rand.node1 <- rnorm(n = nrow(n.node1), mean = mean.node1, sd=sd.est)
 rand.node2 <- rnorm(n = nrow(n.node2), mean = mean.node2, sd=sd.est)
 rand.node3 <- rnorm(n = nrow(n.node3), mean = mean.node3, sd=sd.est)
@@ -26,3 +29,9 @@ hist(fhs$FVC)
 
 hist(n.node4$FVC)
 hist(rand.node4)
+
+# 2 
+fhs.tree <- tree(dth ~ ., data = fhs)
+summary(fhs.tree)
+plot(fhs.tree)
+text(fhs.tree, pretty = 0)
