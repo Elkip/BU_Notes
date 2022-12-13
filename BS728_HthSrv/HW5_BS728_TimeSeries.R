9ed4afc89f84c4554fc25b8d8e67d6ee534ddc7e
Mitchell Henschel <me@mitchellhenschel.com>
2022-12-12 21:11:46 -0500

    728 HW

--- BS728_HthSrv/HW5_BS728_TimeSeries.R
+++ BS728_HthSrv/HW5_BS728_TimeSeries.R
@@ -12,61 +12,71 @@ num.months
 
 attach(deaths)
 
-# Plot the series
+# 2. Plot the series
 # Explain the parts of this code and what each piece is doing.
 plot(deaths$num,type='b',pch=20,ylab="Deaths",xlab="Time",xaxt='n')
 axis(1,at=c(0,12,24,36,48,60),labels=c("1972","1973","1974","1975","1976","1977"))
 
-
-# Investigate seasonality
+# 3. Investigate seasonality
 plot(num~as.factor(num.month),xlab="Month")
-
-# What is this plot showing? 
-# How do you interpret the plot (i.e. what are its implications for modeling?)?
-
 
 # Remove seasonal components
 period1 <- 12
 period2 <- 6
 
 # create sinusoid terms for the two periods you just created.
-
-
+s1 <- sin(c(1:num.months)*2*pi/period1)
+c1 <- cos(c(1:num.months)*2*pi/period1)
+s2 <- sin(c(1:num.months)*2*pi/period2)
+c2 <- cos(c(1:num.months)*2*pi/period2)
 
 # Determine which sinusoids to keep. Justify your decision with graphics.
-
+par(mfrow=c(1,2))
+plot(s1, type='b')
+plot(deaths$num,type='b',pch=20,ylab="Deaths",xlab="Time")
+plot(s2, type = 'b')
+plot(deaths$num,type='b',pch=20,ylab="Deaths",xlab="Time")
+plot(c1, type = 'b')
+plot(deaths$num,type='b',pch=20,ylab="Deaths",xlab="Time")
+plot(c2, type = 'b')
+plot(deaths$num,type='b',pch=20,ylab="Deaths",xlab="Time")
 
 # Plot the original data with the fitted values from the chosen model overlaid in a 
 #   different color.
+par(mfrow=c(1,1))
+fit1 <- lm(num~s1+c1)
+plot(fit1$residuals,type='l')
+lines(lin.reg2$fitted.values,col="blue",lwd=3)
 
 
-
-#################
-# FIT THE TREND #
-#################
+# 4. FIT THE TREND
 # Look at the residuals from the model that you just fit.  
-#  Does there appear to be a trend in these residuals?
+resids <- fit1$residuals
+plot(fit1$residuals)
 
 #  Now, try to fit a linear trend using the time variable as the predictor. 
-	# Hint: you will want to use the function lm().
+lin.reg <- lm(resids~time)
+summary(lin.reg)
+plot(lin.reg$residuals,type='l') 
 
-
+lin.reg2 <- lm(resids~s1+c1)
+plot(lin.reg2$residuals, type='l')
 #  Now investigate if the trend is quadratic. Fit the appropriate model.
 time2 <- time^2
+qaud.reg <- lm(resids~time2)
+summary(qaud.reg)
+plot(qaud.reg$residuals, type='l')
 
-#  What model seems most appropriate?
-
+quad.reg2 <- lm(resids~s1^2+c1^2)
+plot(quad.reg2$residuals, type='l')
 # Plot the residuals from the seasonally adjusted model. Then overlay a plot of the 
 #  fitted values from the model you just chose in a different color. 
-
-# Get the residuals from this model and call them resids. Plot resids. How do they look? 
-#   Do you feel that all trends and seasonal components have been removed? Why or why not?
-
+plot(fit1$residuals)
+lines(lin.reg2$fitted.values,col="blue",lwd=3)
 
 # Plot the acf of resids. How does it look? What type of model do you think 
-#   that you should use (look at slide 78 of the notes).
-
-
+#   that you should use 
+acf(resids, main="base")
 
 # try a few different models-
 fit.ma6 <- arima(resids,order=c(0,0,6))
@@ -79,9 +89,5 @@ fit.ma12$aic
 
 # Also consider the acfs of the residuals from these models.
 par(mfrow=c(2,1))
-acf(fit.ma6$residuals)
-acf(fit.ma12$residuals)
-
-# The acf for ma(12) appears to be fitting better. What about these plots indicates that?
-
-
+acf(fit.ma6$residuals, main = "order 6")
+acf(fit.ma12$residuals, main = "order 12")
