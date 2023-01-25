2adb0c7a94be0b56fb2bda8edf8564583fd43b85
elkip <mitchellhenschel@gmail.com>
2023-01-25 17:39:50 -0500

    HW1

--- BS849_BayesModelin/BS849_HW1_20230127.R
+++ BS849_BayesModelin/BS849_HW1_20230127.R
@@ -1,24 +1,22 @@
 library("rjags")
 library("coda")
 
+set.seed(0)
+# 2b. Simulate a sample of 10000 values and generate the empirical density
 model_1.tx <- "model {
    theta ~ dbeta(3, 2)
-   Y ~ dbin(theta, 10000) 
    P1 <- step(theta - .8)
-   P2 <- step(Y - 33)
   }"
 model_1 <- jags.model(textConnection(model_1.tx))
 
 update(model_1, n.iter = 1000)
 
-# 2b. Simluate a sample of 10000 values and generate the empirical density
-test_1 <- coda.samples(model_1, c('theta', 'Y'), 
+test_1 <- coda.samples(model_1, c('theta', 'P1'), 
                         n.iter = 10000)
 plot(test_1)
 
 # 2c. Estimate the probability theta > .8
-test2 <- coda.samples(model_1, c("theta", "P1"), n.iter = 1000)
-summary(test2)
+summary(test_1)
 
 # 2e. Estimate the probability in a set of 50, n = 33 (66%)
 model_2.tx <- "model {
@@ -28,5 +26,5 @@ model_2.tx <- "model {
   }"
 model_2 <- jags.model(textConnection(model_2.tx))
 update(model_2, n.iter = 1000)
-test3 <- coda.samples(model_2, c("Y", "P2"), n.iter = 1000)
-summary(test3)
+test2 <- coda.samples(model_2, c("Y", "P2"), n.iter = 1000)
+summary(test2)
