4625e48fee055c1c33925855a1a456ece286a151
elkip <mitchellhenschel@gmail.com>
2023-02-21 23:07:58 -0500

    HW

--- BS857_CorrData/BS857_HW5_20230223.sas
+++ BS857_CorrData/BS857_HW5_20230223.sas
@@ -1,7 +1,10 @@
 libname HW5 'Z:\';
 
 data birth;
-set HW5.birthwt;
+    set HW5.birthwt;
+    by mid;
+    time_men=age - lag(age);
+    if first.mid then time_men=0;
 run;
 
 *1 Fit the mixed effects model with linear trend for 
@@ -11,27 +14,49 @@ model weight=age/s chisq;
 random intercept/type=un subject=mid G V;
 run;
 
-proc mixed data=birth covtest plots=all;
-class order;
-model weight=age/s chisq;
-random intercept/type=un subject=mid G V;
-run;
-
 *2 Fit the fixed effect model with beta-age (FE);
 proc glm data=birth;
 class mid;
-model weight=age;
-run;
-
-proc mixed data=birth covtest plots=all;
-class order;
-model weight=age/s chisq;
-repeated order/type=sp(exp) (order) subject=mid;
-run;
+model weight=age/solution;
+run;quit;
 
 *4 Construct a histogram of standardized residuals along with a QQ plot;
-
+proc mixed data=birth method=ml covtest plots=all;
+model weight = age/s chisq outpred=yhat outpm=pred residual;
+random intercept /type=un subject=mid G V ;
+run;
 
 *5 Are any mothers outliers?;
+data pred;
+set pred;
+sqStudentResid=StudentResid**2;
+visits=1;
+run;
+
+proc means data=pred noprint;
+var sqStudentResid visits;
+output out=mahalanobis (drop=_type_ _freq_)
+sum(sqStudentResid visits)=distance novisits;
+by mid;
+run;
+
+data mahalanobis;
+set mahalanobis;
+pvalue=1-probchi(distance,novisits);
+run;
+
+proc print data=mahalanobis;
+where pvalue<0.05/878;
+run;
 
 *6 Plot the semi-variogram for the standardized residuals and lowess smoothed curve;
+proc variogram data=pred outv=outv noprint;
+compute lagd=1 maxlag=10;
+coord xc=time_men yc=visits;
+by mid;
+var StudentResid;
+run;
+
+proc loess data=outv plots=all;
+   model variog = distance;
+run;
--- Research/osteoarthritis.R
+++ Research/osteoarthritis.R
@@ -32,7 +32,7 @@ c0_df <- clinical0  %>%
 
 data_baseline <- inner_join(e_df, c0_df, by = "ID")  %>%
   select(ID = ID, AGE = V00AGE, SEX = SEX, CEMPLOY = V00CEMPLOY, EDCV = V00EDCV, 
-         MEDINS = V00MEDINS, PASE = V00PASE, P01OAGRD = P01OAGRD,
+         MEDINS = as.factor(V00MEDINS, na.action = na.pass), PASE = V00PASE, P01OAGRD = P01OAGRD,
          P02JBMPCV_NEW = P02JBMPCV_NEW, WOMADL = WOMADL, WOMKP = WOMKP, 
          WOMSTF = WOMSTF, V00WTMAXKG = V00WTMAXKG,  V00WTMINKG = V00WTMINKG, 
          BMI = P01BMI, HEIGHT = P01HEIGHT, WEIGHT = P01WEIGHT, 
@@ -57,35 +57,52 @@ data_baseline <- data_baseline %>%
          RACE_AA = coalesce(if_any(RACE, `==`, 2), 0),
          RACE_NW = coalesce(if_any(RACE, `>`, 2), 0)
          ) %>% 
-  select(-c(CEMPLOY, EDCV, P01OAGRD, P02JBMPCV_NEW, RACE))
+  select(-c(CEMPLOY, EDCV, P01OAGRD, P02JBMPCV_NEW, RACE)) 
 
-# Attach predicted cluster to baseline case data
+num_col <- c(1:2, 5:15)
+fac_col <- c(3, 4, 16:35)
+data_baseline[,num_col] <- sapply(data_baseline[,num_col], as.numeric)
+for (i in fac_col) {
+  print(i)
+  data_baseline[,i] <- sapply(data_baseline[,i], as.factor)
+}
+
+# Attach predicted RF K=4 cluster ID to baseline data
 data_bl_cases <- data_baseline[data_baseline$ID %in% indv_info_clstr$ID,] %>%
-    inner_join(indv_info_clstr[,c(1,43)], by=id)
+  full_join(indv_info_clstr[,c(2,43)], by=NULL)
 data_bl_cntrl <- data_baseline[!(data_baseline$ID %in% indv_info_clstr$ID),]
-final_data_cases <- indv_info_clstr[,c(2:36,43)]
+data_fnl_cases <- indv_info_clstr[,c(2:36,43)]
 
+which(is.na(trn_data), arr.ind = TRUE) # 12 NA values in baseline cases
 
 library(randomForest)
-rf <- randomForest(as.factor(RF.4.Clusters) ~  AGE + SEX + RACE_NW
+trn_data <- rfImpute(data_bl_cases[,2:35], data_bl_cases[,36], data=data_bl_cases)
+colnames(trn_data)[1] <- "Clusters"
+
+set.seed(1)
+rf <- randomForest(as.factor(Clusters) ~  AGE + SEX + RACE_NW
                    + RACE_AA + ETHNICITY + CEMPLOY_NWOR + CEMPLOY_NWH + CEMPLOY_FB 
                    + MEDINS + PASE + WOMADL + WOMKP + WOMSTF + BMI + HEIGHT 
                    + WEIGHT + COMORBSCORE + CESD + NSAID + NARC + P01OAGRD_Severe
-                   + P01OAGRD_Moderate + P01OAGRD_Mild+ P01OAGRD_Possible 
+                   + P01OAGRD_Moderate + P01OAGRD_Mild + P01OAGRD_Possible 
                    + P02JBMPCV_NEW_None + P02JBMPCV_NEW_One + V00EDCV_GradDeg
                    + V00EDCV_SomeGrad + V00EDCV_UGDeg + V00EDCV_SomeUG 
                    + V00EDCV_HSDeg+ V00WTMAXKG + V00WTMINKG + Surg_Inj_Hist, 
-                   data=trn_data)
-table(predict(rf), trn_data$RF.4.Clusters)
+                   data=trn_data, proximity=TRUE)
+plot(rf, log="y")
+varImpPlot(rf)
+table(predict(rf), trn_data$Clusters)
+getTree(rf, 1, labelVar = TRUE)
 
 # Alternative with as.factor
-# rf <- randomForest(as.factor(RF.4.Clusters) ~  AGE + SEX +
+# rf <- randomForest(as.factor(Clusters) ~  AGE + SEX +
 #                      + as.factor(RACE) + as.factor(ETHNICITY) + as.factor(CEMPLOY) + MEDINS + PASE + WOMADL
 #                    + WOMKP + WOMSTF + BMI + HEIGHT + WEIGHT + COMORBSCORE + CESD
 #                    + NSAID + NARC + as.factor(P01OAGRD)
 #                    + as.factor(P02JBMPCV) +  + as.factor(V00EDCV)
 #                    + V00WTMAXKG + V00WTMINKG + Surg_Inj_Hist, 
 #                    data=trn_data, na.action = na.omit)
-
 pred <- predict(rf, newdata = data_bl_cntrl)
 summary(pred)
+
+
